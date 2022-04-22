f954381c4989519739bfa57761a4212eaa0b2cd8
Osman F Bayram <74963545+osbm@users.noreply.github.com>
2022-04-22 15:59:31 +0300

    update

---
+++ entrypoint.py
@@ -0,0 +1,18 @@
+import os
+
+from github import Github
+from github.GithubException import GithubException
+
+# get the token from the environment variable
+token = os.environ['GITHUB_TOKEN']
+
+# get the repo name from the environment variable
+repo_name = os.environ['GITHUB_REPOSITORY']
+print(f"{repo_name = }")
+
+
+# get all branches
+g = Github(token)
+repo = g.get_repo('osbm/osbm-CV')
+branches = repo.get_branches()
+print(f"{branches = }")
