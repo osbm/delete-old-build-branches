27caa36f1bc959739e7cd1282a707651c8188f99
Osman F Bayram <74963545+osbm@users.noreply.github.com>
2022-04-26 01:34:58 +0300

    another step forward

--- entrypoint.py
+++ entrypoint.py
@@ -3,22 +3,18 @@ import os
 from github import Github
 from github.GithubException import GithubException
 
-# get the token from the environment variable
-# token = os.environ['GITHUB_TOKEN']
-
-
-
-# get the repo name from the environment variable
-# repo_name = os.environ['GITHUB_REPOSITORY']
-# print(f"{repo_name = }")
-
-# get all branches
-
 g = Github(os.environ["INPUT_GITHUB_TOKEN"])
-repo = g.get_repo('osbm/osbm-CV')
+repo = g.get_repo("osbm/osbm-CV")
 branches = repo.get_branches()
+branch_names = [branch.name for branch in branches]
+
+branches_to_prune = []
+for branch in branch_names:
+    if branch[:7] == "build-" and branch[7:] not in branch_names:
+        branches_to_prune.append(branch)
+
+
 for branch in branches:
-    print(branch.name)
-
-print(f"{branches = }")
-
+    if branch.name in branches_to_prune:
+        print(f"Deleting branch {branch.name}")
+        print(branch)
