619702678710771e6d354efa808ceb5d1fec0e1b
Osman F Bayram <74963545+osbm@users.noreply.github.com>
2022-04-26 01:54:28 +0300

    why this is not working

--- entrypoint.py
+++ entrypoint.py
@@ -18,8 +18,9 @@ def delete_branch(branch_name):
         print('No such branch', branch_name)
 
 branch_names = [branch.name for branch in branches]
-
+print(f"{branch_names=}")
 build_branches = [branch for branch in branch_names if branch.startswith("build-")]
+print(f"{build_branches=}")
 
 for branch in build_branches:
     if branch[7:] not in branch_names: # these are good branches
