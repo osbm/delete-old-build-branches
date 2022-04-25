61af02878cffdc9b6acf31d72d784d2834d05ae7
Osman F Bayram <74963545+osbm@users.noreply.github.com>
2022-04-25 23:56:27 +0300

    docker entrypoint

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ COPY requirements.txt /requirements.txt
 RUN pip install -r /requirements.txt
 
 COPY entrypoint.py /entrypoint.py
-CMD ["python", "/entrypoint.py"]
+ENTRYPOINT ["python", "/entrypoint.py"]
--- entrypoint.py
+++ entrypoint.py
@@ -4,22 +4,15 @@ from github import Github
 from github.GithubException import GithubException
 
 # get the token from the environment variable
-# token = os.environ['GITHUB_TOKEN']
-for k, v in sorted(os.environ.items()):
-    print(k+':', v)
-print('\n')
-# list elements in path environment variable
-[print(item) for item in os.environ['PATH'].split(';')]
+token = os.environ['GITHUB_TOKEN']
 
-
-# get the repo name from the environment variable
-# repo_name = os.environ['GITHUB_REPOSITORY']
-# print(f"{repo_name = }")
+repo_name = os.environ['GITHUB_REPOSITORY']
+print(f"{repo_name = }")
 
 # get all branches
-"""
+
 g = Github(token)
 repo = g.get_repo('osbm/osbm-CV')
 branches = repo.get_branches()
 print(f"{branches = }")
-"""
+
