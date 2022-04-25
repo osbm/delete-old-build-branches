a0756dfd9a09242d538082a49d4269851fbfddfe
Osman F Bayram <74963545+osbm@users.noreply.github.com>
2022-04-26 01:50:04 +0300

    add final version

--- entrypoint.py
+++ entrypoint.py
@@ -2,19 +2,26 @@ import os
 
 from github import Github
 from github.GithubException import GithubException
+from github.GithubException import UnknownObjectException
 
 g = Github(os.environ["INPUT_GITHUB_TOKEN"])
-repo = g.get_repo("osbm/osbm-CV")
+repo = g.get_repo(os.environ["GITHUB_ACTION_REPOSITORY"])
 branches = repo.get_branches()
+
+# thanks to https://github.com/abulka in
+# https://github.com/PyGithub/PyGithub/issues/1260
+def delete_branch(branch_name):
+    try:
+        ref = repo.get_git_ref(f"heads/{branch_name}")
+        ref.delete()
+    except UnknownObjectException:
+        print('No such branch', branch_name)
+
 branch_names = [branch.name for branch in branches]
 
-branches_to_prune = []
-for branch in branch_names:
-    if branch[:7] == "build-" and branch[7:] not in branch_names:
-        branches_to_prune.append(branch)
+build_branches = [branch for branch in branch_names if branch.startswith("build-")]
 
-
-for branch in branches:
-    if branch.name in branches_to_prune:
-        print(f"Deleting branch {branch.name}")
-        print(branch)
+for branch in build_branches:
+    if branch[7:] not in branch_names: # these are good branches
+        print(f"Deleting branch {branch}")
+        delete_branch(branch)
