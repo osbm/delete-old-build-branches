8cb0b4acbef777481160b6029ec1afb8d1c3af7c
Osman F Bayram <74963545+osbm@users.noreply.github.com>
2022-04-25 22:22:00 +0300

    list all environment variables

--- entrypoint.py
+++ entrypoint.py
@@ -5,13 +5,21 @@ from github.GithubException import GithubException
 
 # get the token from the environment variable
 token = os.environ['GITHUB_TOKEN']
+for k, v in sorted(os.environ.items()):
+    print(k+':', v)
+print('\n')
+# list elements in path environment variable
+[print(item) for item in os.environ['PATH'].split(';')]
+
 
 # get the repo name from the environment variable
-repo_name = os.environ['GITHUB_REPOSITORY']
-print(f"{repo_name = }")
+# repo_name = os.environ['GITHUB_REPOSITORY']
+# print(f"{repo_name = }")
 
 # get all branches
+"""
 g = Github(token)
 repo = g.get_repo('osbm/osbm-CV')
 branches = repo.get_branches()
 print(f"{branches = }")
+"""
