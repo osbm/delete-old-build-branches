8dba3a395a85710f8b80c7c787dff6a7a2156501
Osman F Bayram <74963545+osbm@users.noreply.github.com>
2022-04-26 01:13:53 +0300

    update

--- entrypoint.py
+++ entrypoint.py
@@ -7,12 +7,6 @@ from github.GithubException import GithubException
 # token = os.environ['GITHUB_TOKEN']
 
 
-for k, v in sorted(os.environ.items()):
-    print(k+':', v)
-print('\n')
-# list elements in path environment variable
-[print(item) for item in os.environ['PATH'].split(';')]
-
 
 # get the repo name from the environment variable
 # repo_name = os.environ['GITHUB_REPOSITORY']
@@ -20,7 +14,7 @@ print('\n')
 
 # get all branches
 
-g = Github(token)
+g = Github(os.environ["INPUT_GITHUB_TOKEN"])
 repo = g.get_repo('osbm/osbm-CV')
 branches = repo.get_branches()
 print(f"{branches = }")
